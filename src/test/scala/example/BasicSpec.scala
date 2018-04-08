760efe6b5db1bc28276f59489e0ad0f8416a1930
Richard S. Imaoka <richard.e.imaoka@gmail.com>
2018-04-08 16:46:34 +0900

    Structure the test cases

--- src/test/scala/example/BasicSpec.scala
+++ src/test/scala/example/BasicSpec.scala
@@ -66,13 +66,13 @@ class BasicSpec() extends TestKit(ActorSystem("BasicSpec")) with ImplicitSender
       case (user, list) => user -> countUnreadTopics(list)
     }
 
-  "Cache process" must {
-    /**
-     * In case the whole cache process is restarted, we should be able to query into SQL DB,
-     * and pass all topic-read/unread status for all users, then initialize the cache accordingly
-     *
-     * `val subscriptions` represents the data from SQL DB.
-     */
+  /**
+   * In case the whole cache process is restarted, we should be able to query into SQL DB,
+   * and pass all topic-read/unread status for all users, then initialize the cache accordingly
+   *
+   * `val subscriptions` represents the data from SQL DB.
+   */
+  "SetUnread" must {
     "initialize unread status correctly" in {
       val subscriptions: Map[User, List[(Topic, Boolean)]] = constructSubscription(users)
       val expected = constructCountData(subscriptions)
@@ -82,23 +82,75 @@ class BasicSpec() extends TestKit(ActorSystem("BasicSpec")) with ImplicitSender
         (topic, unreadFlag) <- subscriptions.get(user).get
       } {
         topicService.subscribeTo(topic, user)
-        if(unreadFlag) topicService.setUnread(topic, user)
+        if (unreadFlag) topicService.setUnread(topic, user)
       }
 
       expectMsg(200.milliseconds, expected)
     }
+  }
 
-    "A new subscription will result updated unread count" in {
-      val subscriptions: Map[User, List[(Topic, Boolean)]] = constructSubscription(users)
 
-      val user3 = User("user3")
-      topicService.subscribeTo(Topic("topicA"), user3)
-      topicService.setUnread(Topic("topicA"), user3)
+  "Subscribe" when {
+    "invoked single time" must {
+      "Increase the unread count for a user/topic" in {
+        /**
+         * Only the diff from the previous batch update should be reported
+         */
+          val subscriptions: Map[User, List[(Topic, Boolean)]] = constructSubscription(users)
 
-      val user3Unread = countUnreadTopics(subscriptions.get(user3).get)
-      val expected = Map(user3 -> (user3Unread + 1))
+          val user3 = User("user3")
+          topicService.subscribeTo(Topic("topicA"), user3)
+          topicService.setUnread(Topic("topicA"), user3)
 
-      expectMsg(200.milliseconds, expected)
+          val user3Unread = countUnreadTopics(subscriptions.get(user3).get)
+          val expected = Map(user3 -> (user3Unread + 1))
+
+          expectMsg(200.milliseconds, expected)
+
+      }
+      "does not change anything for already subscribed topic" in {
+        //expectNoMsg
+      }
+    }
+
+    "invoked multiple times" must {
+      "Increase the unread count for multiple users/topics" in {
+      }
     }
   }
+
+  "AllRead" when {
+    "invoked single time" must {
+      "Decrease the unread count for a user/topic" in {
+
+      }
+      "does not change anything for already subscribed topic" in {
+        //expectNoMsg
+      }
+    }
+
+    "invoked multiple times" must {
+      "Decrease the unread count for multiple users/topics" in {
+
+      }
+    }
+  }
+
+  "NewComment" when {
+    "invoked single time" must {
+      "Increase the unread count for all subscribers except the updating user" in {
+
+      }
+      "does not do anything if all subscribers had unread" in {
+
+      }
+    }
+
+    "invoked multiple times" must {
+      "Increase the unread count for all subscribers except the updating user" in {
+
+      }
+    }
+  }
+
 }
